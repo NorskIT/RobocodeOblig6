bca0cd58533440e78ceb89b03eafd09a6e10c2aa
Unknown <magnus.d.pettersen@hotmail.com>
2018-04-10 15:08:35 +0200

    La til mitt første prosjekt

---
+++ Robots/firstRobot/src/no/hiof/skole/robotcodefirst/Main.java
@@ -0,0 +1,8 @@
+package no.hiof.skole.robotcodefirst;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Robots/firstRobot/src/no/hiof/skole/robotcodefirst/termenine.java
@@ -0,0 +1,19 @@
+package no.hiof.skole.robotcodefirst;
+
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class termenine extends Robot {
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+        fire(1);
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            turnRadarLeft(360);
+
+        }
+    }
+}
