a611439eec30aa531c489e3976d1350eb0f4dc88
Unknown <magnus.d.pettersen@hotmail.com>
2018-04-12 14:52:39 +0200

    Auto stash before merge of "Magnus" and "origin/master"

--- Robots/firstRobot/src/no/hiof/skole/robotcodefirst/termenine.java
+++ Robots/firstRobot/src/no/hiof/skole/robotcodefirst/termenine.java
@@ -4,15 +4,45 @@ import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
 public class termenine extends Robot {
+    boolean lookForEnemy = true;
+    boolean turnChooserGun = true;
     @Override
     public void onScannedRobot(ScannedRobotEvent event) {
-        fire(1);
+        lookForEnemy = false;
+        double enemyLocation = getRadarHeading();
+        System.out.println(enemyLocation);
+
+        turnGunLeft(getHeading() - getGunHeading() + event.getBearing());
+        fire(3);
+        
+        if(turnChooserGun) {
+            turnChooserGun = false;
+        }
+        else {
+            turnChooserGun = true;
+        }
+        lookForEnemy = true;
     }
 
     @Override
     public void run() {
+        setAdjustGunForRobotTurn(true);
+
         while(true) {
+<<<<<<< Updated upstream
             turnRadarLeft(360);
+=======
+            if(lookForEnemy) {
+                if(turnChooserGun) {
+                    turnGunRight(360);
+                }
+                else {
+                    turnGunLeft(360);
+                }
+            }
+
+>>>>>>> Stashed changes
         }
+
     }
 }
