1b3dd876a76e161f4af5f8c64b9d96edb40d516c
Unknown <magnus.d.pettersen@hotmail.com>
2018-04-12 15:41:20 +0200

    Created second project

---
+++ Robots/secondRobot/src/no/hiof/skole/robotcodesecond/Shady.java
@@ -0,0 +1,14 @@
+package no.hiof.skole.robotcodesecond;
+
+
+import robocode.Robot;
+
+public class Shady extends Robot{
+
+    @Override
+    public void run() {
+        while(true) {
+            ahead(100);
+        }
+    }
+}
